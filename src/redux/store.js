84f5ef65b547f863b1400139b4e8b9ff68d2c5dd
unknown <houbara0@gmail.com>
2021-04-21 01:59:05 +0300

    5 commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,10 @@ import { persistStore } from "redux-persist";
 import combineReducers from "./root-reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const middelwares = [logger];
+const middelwares = [];
+if ( process.env.NODE_ENV === "development"){
+  middelwares.push(logger)
+}
 export const store = createStore(
   combineReducers,
   composeWithDevTools(applyMiddleware(...middelwares))
